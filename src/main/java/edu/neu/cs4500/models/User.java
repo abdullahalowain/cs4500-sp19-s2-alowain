0c0da83bbacdaa3b1dbbd5b01ead3ae5ab1bc470
Abdullah <alowain.a@husky.neu.edu>
2019-03-27 06:28:00 -0400

    Added user-service and first method

--- src/main/java/edu/neu/cs4500/models/User.java
+++ src/main/java/edu/neu/cs4500/models/User.java
@@ -1,4 +1,4 @@
-package edu.neu.cs4500.controller.hello;
+package edu.neu.cs4500.models;
 
 public class User {
     private Integer id;
@@ -6,6 +6,15 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+
+    public User(int i, String username, String password, String firstName, String lastName) {
+        this.id = i;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     // provide a public set and get method for each of the class variables above
     // make sure to use proper naming conventions for Java methods, e.g.,
     // methods should be camelcase. Here's an example using the firstName variable:
